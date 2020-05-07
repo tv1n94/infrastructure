cdaf5d7f65c97a60ad06a8258076a25666c1f4b2
Alex <aliesha1994@rambler.ru>
2020-05-07 16:46:38 +0000

    Edit configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -3,12 +3,13 @@ set -e
 
 #Установка Ruby и bundler
 apt-get update
-apt-get install -y ruby-full build-essential git
+apt-get install -y ruby-full build-essential git wget
 gem install --no-rdoc --no-ri bundler
 
 #Установка mongodb
-wget -qO -  https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" > /et$
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo apt-get install gnupg
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
 apt-get install -y mongodb-org
 
